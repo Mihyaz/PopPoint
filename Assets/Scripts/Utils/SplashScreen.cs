1db48eb799c73d87b42a476c809d9c915888e184
Mihyaz <40426286+Mihyaz@users.noreply.github.com>
2020-04-06 00:23:27 +0300

    Update SplashScreen.cs

--- Assets/Scripts/Utils/SplashScreen.cs
+++ Assets/Scripts/Utils/SplashScreen.cs
@@ -12,11 +12,6 @@ public class SplashScreen : MonoBehaviour
 
     IEnumerator LoadYourAsyncScene()
     {
-        // The Application loads the Scene in the background as the current Scene runs.
-        // This is particularly good for creating loading screens.
-        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
-        // a sceneBuildIndex of 1 as shown in Build Settings.
-
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
 
         // Wait until the asynchronous scene fully loads
